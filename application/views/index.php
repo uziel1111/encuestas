94dcbdf39eaec4c0c6830aab79c73b9e8b199672
uziel1111 <uziel.123@gmail.com>
2020-01-09 16:32:20 -0600

    cambios estylos

--- application/views/index.php
+++ application/views/index.php
@@ -3,17 +3,17 @@
 
   <div class="row no-gutters justify-content-md-center mt-4">
     <div class="col-md-5 info-img-1 shadow text-justify">
-	
-        <p class="card-text">
+
+        <p class="card-text font-weight-bold">
 			La educación debe ser una oportunidad para todos a lo largo de la vida. En la Secretaría de Educación Pública y Cultura de Sinaloa, y el Instituto Sinaloense para la Educación de los Adultos (ISEA), queremos apoyar a todas las personas que no han concluido la educación básica.
 		</p>
-        <p class="card-text">
-			La escuela es una instancia privilegiada para ayudar a los adultos en este objetivo. Por ello, invitamos a toda la comunidad escolar a que pregunte a los alumnos y personal de la escuela si tienen algún familiar, amigo o conocido que sea mayor de edad y que no haya concluido la primaria o la secundaria. No importa en qué parte del estado viva esa persona. 
+        <p class="card-text font-weight-bold">
+			La escuela es una instancia privilegiada para ayudar a los adultos en este objetivo. Por ello, invitamos a toda la comunidad escolar a que pregunte a los alumnos y personal de la escuela si tienen algún familiar, amigo o conocido que sea mayor de edad y que no haya concluido la primaria o la secundaria. No importa en qué parte del estado viva esa persona.
 		</p>
-		<p class="card-text">
-			Lo único que hay que hacer es registrar a estas personas en esta aplicación, para que un representante del ISEA se ponga en contacto con ellos. Al registrarla, le habrás ayudado a esa persona a alcanzar una meta que seguramente tendrá un impacto muy positivo en su vida y en la de su familia. 
+		<p class="card-text font-weight-bold">
+			Lo único que hay que hacer es registrar a estas personas en esta aplicación, para que un representante del ISEA se ponga en contacto con ellos. Al registrarla, le habrás ayudado a esa persona a alcanzar una meta que seguramente tendrá un impacto muy positivo en su vida y en la de su familia.
 		</p>
-		<p class="card-text">
+		<p class="card-text font-weight-bold">
 			¡Todos son bienvenidos!
 		</p>
     </div>

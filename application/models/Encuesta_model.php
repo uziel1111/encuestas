d6b4219eb82ab848de8682b774a68cbbf46718ee
uziel1111 <uziel.123@gmail.com>
2020-03-09 10:55:29 -0600

    ajuste f registros encuestas y fmod

--- application/models/Encuesta_model.php
+++ application/models/Encuesta_model.php
@@ -20,22 +20,23 @@ class Encuesta_model extends CI_Model
     	if(count($registro) > 0){
     		$str_query2 = "UPDATE sin_registro SET registro = ?, fec_ultimoreg = ? WHERE id_cct = ?";
     		return $this->db->query($str_query2, array($status, $fcreacion, $id_cct));
-    	}else{	
+    	}else{
     		$str_query2 = "INSERT INTO sin_registro(id_cct, registro, fec_ultimoreg) VALUES(?, ?, ?)";
     		return $this->db->query($str_query2, array($id_cct, $status, $fcreacion));
     	}
     }
 
     function set_encuesta($idcct, $cct, $respuestas, $idencuesta){
+      $fcreacion = date("Y-m-d H:i:s");
     	$this->db->trans_start();
-    	$str_query = "INSERT INTO encuesta_x_cct (id_cct, id_encuesta) VALUES(?, ?);";
-		$this->db->query($str_query, array($idcct, $idencuesta));
+    	$str_query = "INSERT INTO encuesta_x_cct (id_cct, id_encuesta, f_registro) VALUES(?, ?, ?);";
+		$this->db->query($str_query, array($idcct, $idencuesta, $fcreacion));
 		$ultimoId = $this->db->insert_id();
 		foreach ($respuestas as $respuesta) {
 			$str_query = "INSERT INTO respuesta (respuesta, id_pregunta, id_aplica) VALUES('{$respuesta['respuesta']}', {$respuesta['id_pregunta']}, {$ultimoId});";
 			$this->db->query($str_query);
 		}
-		
+
 		$this->db->trans_complete();
 
 	    if ($this->db->trans_status() === FALSE)
@@ -55,14 +56,14 @@ class Encuesta_model extends CI_Model
     }
 
     function delete_encuestaxcct($id_encuesta){
-    
+
 		$this->db->trans_start();
     	$str_query = "DELETE FROM respuesta WHERE id_aplica = ?";
 		$this->db->query($str_query, array($id_encuesta));
 
 		$str_query = "DELETE FROM encuesta_x_cct WHERE id_aplica = ?";
 		$this->db->query($str_query, array($id_encuesta));
-		
+
 		$this->db->trans_complete();
 
 	    if ($this->db->trans_status() === FALSE)
@@ -80,7 +81,10 @@ class Encuesta_model extends CI_Model
     }
 
     function set_encuesta_edit($respuestas, $id_editando){
+      $fmod = date("Y-m-d H:i:s");
     	$this->db->trans_start();
+      $str_query1 = "UPDATE encuesta_x_cct SET f_mod = ? WHERE id_aplica = ?;";
+		  $this->db->query($str_query1, array($fmod, $id_editando));
 			foreach ($respuestas as $respuesta) {
 				$str_query = "UPDATE respuesta SET respuesta = '{$respuesta['respuesta']}' WHERE id_pregunta = {$respuesta['id_pregunta']} AND id_aplica ={$id_editando}";
 				$this->db->query($str_query);
@@ -93,7 +97,7 @@ class Encuesta_model extends CI_Model
 	    }else{
 	        return true;
 	    }
-    	
+
     }
 
     function sin_registros_update($id_cct, $status){
@@ -105,7 +109,7 @@ class Encuesta_model extends CI_Model
     	if(count($registro) > 0){
     		$str_query2 = "UPDATE sin_registro SET registro = ?, fec_ultimoreg = ? WHERE id_cct = ?";
     		return $this->db->query($str_query2, array($status, $fcreacion, $id_cct));
-    	}else{	
+    	}else{
     		$str_query2 = "INSERT INTO sin_registro(id_cct, registro, fec_ultimoreg) VALUES(?, ?, ?)";
     		return $this->db->query($str_query2, array($id_cct, $status, $fcreacion));
     	}
@@ -115,7 +119,7 @@ class Encuesta_model extends CI_Model
     	$str_query = "SELECT x.*, m.municipio AS nom_municipio
 						FROM(
 						SELECT
-						sr.fec_ultimoreg, en.id_aplica, ct.cct, ct.turno, ct.nombre_ct, 
+						en.f_registro as fec_ultimoreg, en.id_aplica, ct.cct, ct.turno, ct.nombre_ct,
 						MAX(CASE WHEN p.id_pregunta = 18 THEN r.respuesta ELSE '' END) AS 'NOMBRE(S)',
 						MAX(CASE WHEN p.id_pregunta = 19 THEN r.respuesta ELSE '' END) AS 'PRIMER APELLIDO',
 						MAX(CASE WHEN p.id_pregunta = 20 THEN r.respuesta ELSE '' END) AS 'SEGUNDO APELLIDO',
@@ -130,7 +134,6 @@ class Encuesta_model extends CI_Model
 						INNER JOIN cct ct ON en.id_cct=ct.id_cct
 						INNER JOIN respuesta r ON en.id_aplica= r.id_aplica
 						INNER JOIN pregunta p ON r.id_pregunta = p.id_pregunta
-						LEFT JOIN sin_registro sr on en.id_cct = sr.id_cct
 						GROUP BY r.id_aplica
 						ORDER BY en.id_aplica, ct.cct, ct.id_turno) AS x
 						INNER JOIN municipio m ON x.MUNICIPIO=m.id_municipio";
